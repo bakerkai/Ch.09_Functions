7917b1fda26d0c7686da15f27a04489a36c5a5c4
bakerkai <bakerkai@urbandaleschools.com>
2022-01-04 13:53:55 -0600

    h

--- FunctionPractice.py
+++ FunctionPractice.py
@@ -1,6 +1,7 @@
 import math
 
 def volume_sphere(radius):
+    '''this function calculates the volume of the sphere'''
     volume = 4/3 * math.pi *math.pow(radius,3)
     print(f"The volume of the sphere is... {volume:.2f}")
 
@@ -10,6 +11,31 @@ def volume_scyl(r,h):
     volume =math.pi *math.pow(r,2)*h
     return volume
 
-volume_sphere(4)
-vol = 24*volume_scyl(2,3)
-print(vol)
+def hyp(leg1,leg2):
+    hypot = math.sqrt(leg1**2 + leg2**2)
+    print(hypot)
+
+def mean(list):
+    total =0
+    for item in list:
+        total += item
+    avg = total/len(list)
+    print(avg)
+
+def perimeter(b,h):
+    per = 2*b +2*h
+    print(per)
+
+def myprogram():
+    hyp(1,2)
+    numlist=[13,2,54,23,34,71]
+    mean(numlist)
+
+print(__name__)
+if __name__ == "__main__":
+    myprogram()
+'''
+ volume_sphere(4)
+    vol24 = 24 * volume_scyl(2, 3)
+    print(vol24)
+    '''
